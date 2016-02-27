38e49c06bea12d055d42467169205a102a0d7a1e
Aku Kolu <Accu@Akus-MBP.home>
2016-02-27 22:05:51 +0200

    add config.js to gulpfile

---
+++ compiled/traceur/config.js
@@ -0,0 +1,39 @@
+"use strict";
+var env = process.env.NODE_ENV || 'development';
+var config = {
+  development: {
+    irc: {
+      server: 'irc.elisa.fi',
+      nick: 'VisionBot',
+      options: {
+        channels: ['#nsfwbot'],
+        port: 6667
+      }
+    },
+    labelThreshold: 0.8,
+    safeSearchTolerance: {
+      adult: 'POSSIBLE',
+      spoof: '',
+      medical: 'POSSIBLE',
+      violence: 'VERY_LIKELY'
+    }
+  },
+  production: {
+    irc: {
+      server: 'irc.ihme.org',
+      nick: 'VisionBot',
+      options: {
+        channels: ['#ihme'],
+        port: 6667
+      }
+    },
+    labelThreshold: 0.8,
+    safeSearchTolerance: {
+      adult: 'POSSIBLE',
+      spoof: '',
+      medical: 'POSSIBLE',
+      violence: 'VERY_LIKELY'
+    }
+  }
+};
+module.exports = config[env];
--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,9 @@ var gulp = require('gulp');
 var traceur = require('gulp-traceur');
 
 gulp.task('traceur', function () {
-  gulp.src(['./app.js'])
+  gulp.src(['./app.js', './config.js'])
     .pipe(traceur({ blockBinding: true }))
     .pipe(gulp.dest('./compiled/traceur'));
 });
 
 gulp.task('default', ['traceur']);
-
